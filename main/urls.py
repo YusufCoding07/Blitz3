cf650ab92a626aad932ab3fa964e575ebc82f57f
yusuf <yusufmallam@gmail.com>
2025-02-04 22:24:59 +0000

    new

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
+# main/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Root URL
+    path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
+    path('transactions/', views.transactions, name='transactions'),  # Add this line
     path('map/', views.map_view, name='map'),
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,7 @@ def profile(request):
 # Map Page
 def map_view(request):
     return render(request, 'main/map.html')
+
+def transactions(request):
+    transactions = Transaction.objects.filter(user=request.user)  # Example query
+    return render(request, 'main/transactions.html', {'transactions': transactions})
