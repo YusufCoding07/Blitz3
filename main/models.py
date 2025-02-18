161f5ac80d55e03a49f7601de3167d755c9aee7e
yusuf <yusufmallam@gmail.com>
2025-02-18 20:29:09 +0000

    new

--- main/models.py
+++ main/models.py
@@ -1,24 +1,17 @@
-﻿from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import User
+﻿from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20, blank=True)
-    profile_picture = models.ImageField(
-        upload_to='profile_pics/', 
-        blank=True,
-        default='profile_pics/default.png'
-    )
+    profile_picture = CloudinaryField('image', blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
     is_driver = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
-    
-from django.db import models
-from django.contrib.auth.models import User
+        return f'{self.user.username} Profile'
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -28,26 +21,12 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"Transaction {self.id} by {self.user.username}"
-    
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-    
-
-from cloudinary.models import CloudinaryField
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = CloudinaryField('image', blank=True, null=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
+    instance.userprofile.save()
