c366f440918cdd29279c5b82098a4d48b79a3c05
yusuf <yusufmallam@gmail.com>
2025-02-18 22:17:00 +0000

    new

--- main/forms.py
+++ main/forms.py
@@ -2,30 +2,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile  # Add this import
+from .models import UserProfile
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    profile_picture = forms.ImageField(required=False)
-    phone_number = forms.CharField(max_length=20, required=False)
-    
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
-        
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['email'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
-        self.fields['profile_picture'].widget.attrs.update({'class': 'form-control'})
-        self.fields['phone_number'].widget.attrs.update({'class': 'form-control'})
+        fields = ("username", "email", "password1", "password2")
 
-class ProfileUpdateForm(forms.ModelForm):
+class UserProfileForm(forms.ModelForm):
     class Meta:
-        model = UserProfile  # ✅ Changed from Profile to UserProfile
-        fields = ['profile_picture', 'phone_number']  # Update fields as needed
+        model = UserProfile
+        fields = ['profile_picture', 'phone_number']
 
 class DriverApplicationForm(forms.ModelForm):
     has_valid_license = forms.BooleanField(
