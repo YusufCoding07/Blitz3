6350e56e10959493a746b9759fcc865c886e4959
yusuf <yusufmallam@gmail.com>
2025-02-18 20:32:29 +0000

    new

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Profile, Transaction
+from .models import UserProfile, Transaction
 
 # Register your models
-admin.site.register(Profile)
+admin.site.register(UserProfile)
 admin.site.register(Transaction)
