dc879fbcc56fe48768ab40b226a1da7680509fc6
yusuf <yusufmallam@gmail.com>
2025-02-19 15:41:50 +0000

    new

--- main/migrations/0004_fix_userprofile_table.py
+++ main/migrations/0004_fix_userprofile_table.py
@@ -1,1 +1,17 @@
- 
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0003_fix_table_names'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            ALTER TABLE IF EXISTS main_profile 
+            RENAME TO main_userprofile;
+            """,
+            reverse_sql=migrations.RunSQL.noop
+        ),
+    ] 
