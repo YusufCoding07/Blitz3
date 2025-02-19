0255985a8d2bae64742327f1d3d29d84e67292e4
yusuf <yusufmallam@gmail.com>
2025-02-19 15:30:24 +0000

    new

--- main/migrations/0003_fix_table_names.py
+++ main/migrations/0003_fix_table_names.py
@@ -1,1 +1,16 @@
- 
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0002_alter_transaction_options_alter_userprofile_options'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            DROP TABLE IF EXISTS main_profile CASCADE;
+            """,
+            reverse_sql=migrations.RunSQL.noop
+        ),
+    ] 
