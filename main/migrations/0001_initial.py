a4d47c3911a72920bad0b01ee07a6b32223d001c
yusuf <yusufmallam@gmail.com>
2025-02-19 16:00:34 +0000

    new

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-import cloudinary.models
 
 class Migration(migrations.Migration):
 
@@ -12,27 +11,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            # Drop tables if they exist
-            """
-            DROP TABLE IF EXISTS main_profile CASCADE;
-            DROP TABLE IF EXISTS main_userprofile CASCADE;
-            DROP TABLE IF EXISTS main_transaction CASCADE;
-            """,
-            reverse_sql=migrations.RunSQL.noop
-        ),
         migrations.CreateModel(
             name='UserProfile',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('profile_picture', cloudinary.models.CloudinaryField(blank=True, max_length=255, null=True, verbose_name='image')),
-                ('phone_number', models.CharField(blank=True, max_length=20, null=True)),
-                ('is_driver', models.BooleanField(default=False)),
-                ('has_valid_license', models.BooleanField(default=False)),
-                ('car_model', models.CharField(blank=True, max_length=100, null=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='userprofile', to=settings.AUTH_USER_MODEL)),
+                ('bio', models.TextField(blank=True, max_length=500)),
+                ('profile_pic', models.ImageField(blank=True, null=True, upload_to='profile_pics/')),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
+                'verbose_name': 'User Profile',
+                'verbose_name_plural': 'User Profiles',
                 'db_table': 'main_userprofile',
             },
         ),
@@ -41,12 +30,14 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('description', models.CharField(max_length=255)),
+                ('description', models.CharField(max_length=200)),
                 ('date', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'db_table': 'main_transaction',
+                'verbose_name': 'Transaction',
+                'verbose_name_plural': 'Transactions',
+                'ordering': ['-date'],
             },
         ),
     ] 
