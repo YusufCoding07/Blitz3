1993d3c926acbc7ad47d24c73c4525e14e2a7356
yusuf <yusufmallam@gmail.com>
2025-02-19 16:56:55 +0000

    new

--- main/signals.py
+++ main/signals.py
@@ -6,12 +6,12 @@ from .models import UserProfile
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+    if created:  # Only create profile for new users
+        UserProfile.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     try:
         instance.userprofile.save()
     except UserProfile.DoesNotExist:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.get_or_create(user=instance)
