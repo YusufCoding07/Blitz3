c8f4b2f60785892aa1e6282dd938207c267d3ca2
YusufCoding07 <yusufmallam07@gmail.com>
2025-02-21 17:51:22 +0000

    new

--- main/migrations/0003_update_transaction_model.py
+++ main/migrations/0003_update_transaction_model.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0002_add_driver_application_fields'),
+        ('main', '0001_initial'),
     ]
 
     operations = [
