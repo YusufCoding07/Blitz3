8676b51e0f0469a358147d6492ac79fa39b2f1d0
YusufCoding07 <yusufmallam07@gmail.com>
2025-02-22 19:23:55 +0000

    new

--- main/views.py
+++ main/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RideSearchForm
+from .models import Ride
 
 def home(request):
-    return render(request, 'home.html')
+    rides = Ride.objects.all()[:5]  # Example query
+    return render(request, 'home.html', {'rides': rides})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+def find_ride(request):
+    form = RideSearchForm()
+    return render(request, 'find_ride.html', {'form': form})
