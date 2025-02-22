fe1b82e3d5b5bde97913be80881d73f3432bd88a
YusufCoding07 <yusufmallam07@gmail.com>
2025-02-22 18:23:32 +0000

    new

--- main/views.py
+++ main/views.py
@@ -1,25 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib import messages
-from django.utils import timezone
-from django.db.models import Q, Sum, Avg
-from django.db import transaction, IntegrityError
-from decimal import Decimal
-from datetime import datetime
 
-from .models import Transaction, UserProfile, Ride
-from .forms import (
-    UserRegisterForm,
-    UserProfileForm,
-    DriverApplicationForm,
-    RideCreateForm,
-    RideSearchForm,
-    SignUpForm,
-    ProfileUpdateForm
-)
+def home(request):
+    return render(request, 'main/home.html')
 
-import logging
-logger = logging.getLogger('django')
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
