a015b5fa278b85be61fef92e1b2497b08a58b697
YusufCoding07 <yusufmallam07@gmail.com>
2025-02-22 17:42:43 +0000

    new

--- main/models.py
+++ main/models.py
@@ -72,8 +72,6 @@ class Ride(models.Model):
     time = models.TimeField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     status = models.CharField(max_length=20, choices=RIDE_STATUS, default='available')
-    created_at = models.DateTimeField(auto_now_add=True)
-    notes = models.TextField(blank=True)
     driver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rides_as_driver', null=True, blank=True)
     passenger = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rides_as_passenger', null=True, blank=True)
 
